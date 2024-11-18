84e0a5ac7aa65bcdb82842c72cabd8dfc512d540
Stepan Polyakov <PCA12345@Yandex.ru>
2024-11-18 16:54:18 +0300

    Initial Commit

---
+++ GymBro/GymBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBroApp.swift
+//  GymBro
+//
+//  Created by Stepan Polyakov on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GymBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
