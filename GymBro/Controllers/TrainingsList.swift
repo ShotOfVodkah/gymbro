40c858903cc71a5205cb9832db15887a3344c493
ShotOfVodkah <PCA12345@Yandex.ru>
2025-02-03 16:25:38 +0300

    InfoTab

--- GymBro/Controllers/TrainingsList.swift
+++ GymBro/Controllers/TrainingsList.swift
@@ -11,11 +11,17 @@ import FirebaseFirestoreCombineSwift
 
 struct TrainingsList: View {
     @FirestoreQuery(collectionPath: "workouts") var workouts: [Workout]
+    
     @State private var offset: CGFloat = -400
     @State private var isActive: Bool = false
+    
     @Binding var bar: Bool
+    
     @State private var selectedWorkout: Workout?
     @State private var showWorkoutInfo: Bool = false
+    
+    @State private var search: String = ""
+    
     var body: some View {
         NavigationStack {
             ZStack {
@@ -49,11 +55,19 @@ struct TrainingsList: View {
                     }
                     .padding(.horizontal, 20)
                     
-                    Spacer().frame(height: 30)
+                    TextField("Search", text: $search)
+                        .padding(10)
+                        .background(Color("TabBar"))
+                        .cornerRadius(20)
+                        .overlay(RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color("PurpleColor").opacity(0.6), lineWidth: 3)
+                        )
+                        .padding(.horizontal, 20)
+                        .padding(.bottom, 10)
                     
                     ScrollView(showsIndicators: false) {
                         VStack(spacing: 30) {
-                            ForEach(workouts) { workout in
+                            ForEach(filtered) { workout in
                                 Button {
                                     selectedWorkout = workout
                                     showWorkoutInfo = true
@@ -87,6 +101,13 @@ struct TrainingsList: View {
         }
     }
 
+    var filtered: [Workout] {
+        if search.isEmpty {
+            return workouts
+        } else {
+            return workouts.filter { $0.name.localizedCaseInsensitiveContains(search) }
+        }
+    }
 }
 
 #Preview {
--- GymBro/Controllers/WorkoutInfo.swift
+++ GymBro/Controllers/WorkoutInfo.swift
@@ -12,6 +12,23 @@ struct WorkoutInfo: View {
     @Environment(\.dismiss) private var dismiss
     var workout: Workout
     
+    let mGroups: [String] = ["Chest", "Back", "Buttocks", "Lower legs", "Arms", "Upper legs", "Shoulders"]
+    var percentages: [Double] {
+        var counts = [String: Int]()
+        for exercise in workout.exercises {
+            counts[exercise.muscle_group, default: 0] += 1
+        }
+        return mGroups.map { group in
+            let count = counts[group] ?? 0
+            return workout.exercises.count > 0 ? Double(count) / Double(workout.exercises.count) : 0
+        }
+    }
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 16), // Первая колонка
+        GridItem(.flexible(), spacing: 16)  // Вторая колонка
+    ]
+    
     var body: some View {
         ZStack(alignment: .topLeading) {
             Color("TabBar").ignoresSafeArea(edges: .all)
@@ -19,100 +36,141 @@ struct WorkoutInfo: View {
             VStack(){
                 Circle()
                     .fill(
-                                LinearGradient(
-                                    gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
-                                    startPoint: .leading,
-                                    endPoint: .trailing
-                                )
-                            )
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
                     .offset(x: 0, y: 300)
                     .frame(width: 600, height: 600)
                 Circle()
                     .fill(
-                                LinearGradient(
-                                    gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
-                                    startPoint: .leading,
-                                    endPoint: .trailing
-                                )
-                            )
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
                     .offset(x: 190, y: 300)
                     .frame(width: 200, height: 200)
                 Circle()
                     .fill(
-                                LinearGradient(
-                                    gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
-                                    startPoint: .leading,
-                                    endPoint: .trailing
-                                )
-                            )
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
                     .offset(x: -250, y: 200)
                     .frame(width: 200, height: 200)
                 Circle()
                     .fill(
-                                LinearGradient(
-                                    gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
-                                    startPoint: .leading,
-                                    endPoint: .trailing
-                                )
-                            )
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
                     .offset(x: 50, y: 200)
                     .frame(width: 200, height: 200)
             }
             .frame(width: UIScreen.main.bounds.width, height: 100)
             .opacity(opacity)
-            ZStack {
-                Blur()
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .padding(.horizontal, 25)
-                    .padding(.vertical, 120)
-                    .offset(y: 100)
-
-                ScrollView(showsIndicators: false) {
-                    VStack(spacing: 10) {
-                        ForEach(workout.exercises, id: \.self) { exercise in
-                            HStack {
-                                Image(exercise.muscle_group)
-                                    .frame(width: 30, height: 30)
-                                    .scaleEffect(0.1)
-                                    .padding(.trailing, 10)
-                                Text(exercise.name)
-                                    .font(.system(size: 25))
-                            }
-                            .frame(width: 300)
-                            .padding()
-                            .background(Color("Background").opacity(0.9))
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                            .foregroundColor(Color("TitleColor"))
-                        }
+            VStack {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white)
+                        
                     }
-                    .padding()
-                }
-                .offset(y: 84)
-                .padding(.horizontal, 30)
-                .frame(width: 300, height: 600)
-            }
-            HStack {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.left")
-                        .font(.system(size: 30))
+                    .padding(.leading, 30)
+                    Spacer()
+                    Text(workout.name)
+                        .font(.system(size: 25))
+                        .fontWeight(.semibold)
+                        .padding(.trailing, 60)
                         .foregroundColor(.white)
                     
+                    Spacer()
                 }
-                .padding(.leading, 30)
-                Spacer()
-                Text(workout.name)
-                    .font(.system(size: 25))
-                    .fontWeight(.semibold)
-                    .padding(.trailing, 60)
-                    .foregroundColor(.white)
+                .padding(.top, 65)
                 
-                Spacer()
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(Array(zip(mGroups, percentages)), id: \.0) { group, percentage in
+                        
+                        VStack(spacing: 2) {
+                            Text(group)
+                                .font(.system(size: 15))
+                                .foregroundColor(.white)
+                            
+                            GeometryReader { geometry in
+                                ZStack(alignment: .leading) {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color("TabBar"))
+                                        .frame(width: geometry.size.width, height: 8)
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color("TitleColor"))
+                                        .frame(width: geometry.size.width * CGFloat(percentage), height: 8)
+                                }
+                            }
+                            .frame(height: 8)
+                        }
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 10)
+                
+                ZStack {
+                    Blur()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .padding(.horizontal, 25)
+                        .padding(.vertical, 20)
+                    
+                    ScrollView(showsIndicators: false) {
+                        VStack(spacing: 10) {
+                            ForEach(workout.exercises, id: \.self) { exercise in
+                                HStack {
+                                    Image(exercise.muscle_group)
+                                        .frame(width: 30, height: 30)
+                                        .scaleEffect(0.1)
+                                        .padding(.trailing, 10)
+                                    Text(exercise.name)
+                                        .font(.system(size: 25))
+                                }
+                                .frame(width: 300)
+                                .padding()
+                                .background(Color("Background").opacity(0.9))
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .foregroundColor(Color("TitleColor"))
+                            }
+                        }
+                        .padding()
+                    }
+                    .padding(.horizontal, 30)
+                    .frame(width: 300, height: 530)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "play.fill")
+                            .foregroundColor(.white)
+                            .scaleEffect(2.2)
+                            .frame(width: 80, height: 80)
+                            .background(Circle().fill(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [Color("PurpleColor"), Color.purple]),
+                                    startPoint: .leading,
+                                    endPoint: .trailing
+                                )
+                            ))
+                    }
+                    .offset(y: 250)
+                }
             }
-            .padding(.top, 65)
-            
-            
         }
         .ignoresSafeArea(edges: .all)
         .background(Color("TabBar").opacity(opacity))
