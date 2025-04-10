797b7132f6ea73b52277cd7b75ec0938671709a5
ShotOfVodkah <PCA12345@Yandex.ru>
2025-04-10 14:14:36 +0300

    Apple watch interface

---
+++ GymBro Watch Watch App/WorkoutInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutInfoView.swift
+//  GymBro Watch Watch App
+//
+//  Created by Stepan Polyakov on 10.04.2025.
+//
+
+import SwiftUI
+
+struct WorkoutInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutInfoView()
+}
---
+++ GymBro Watch Watch App/WorkoutPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutPlayerView.swift
+//  GymBro Watch Watch App
+//
+//  Created by Stepan Polyakov on 10.04.2025.
+//
+
+import SwiftUI
+
+struct WorkoutPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutPlayerView()
+}
